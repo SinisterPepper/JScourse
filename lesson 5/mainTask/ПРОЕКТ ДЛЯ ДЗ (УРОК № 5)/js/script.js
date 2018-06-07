e248e7fa3004d842bfa9e61ccc6eb41dd5975149
Roman <roaltih@mail.ru>
2018-06-07 22:17:04 +0400

    Сделал правки по замечаниям в основном задании

--- lesson 5/mainTask/ПРОЕКТ ДЛЯ ДЗ (УРОК № 5)/js/script.js
+++ lesson 5/mainTask/ПРОЕКТ ДЛЯ ДЗ (УРОК № 5)/js/script.js
@@ -7,10 +7,10 @@ let budget,
 function start(){
 	budget = prompt("Ваш бюджет на месяц?", "");
 
-	while(isNaN(budget) || budget === '' || budget == null ){
+	while(isNaN(budget) || budget === '' || budget === null ){
 		budget = prompt("Ваш бюджет?", "");
 	}
-	storeName = prompt("Название вашего магазина?", '').toUpperCase();;
+	storeName = prompt("Название вашего магазина?", '').toUpperCase();
 	
 	time = 19;
 }
@@ -31,7 +31,7 @@ var mainList = {
 				for (var i = 0; i < 5; i++){
 					var good = prompt("Какой тип товаров будем продавать?", "");
 					
-					if((typeof(good)) === 'string' && (typeof(good)) != null && good != '' && good.length < 50){
+					if((typeof(good)) === 'string' && (typeof(good)) !== null && good !== '' && good.length < 50){
 						console.log("Все верно!");
 						mainList.shopGoods[i] = good;
 					} else {
@@ -62,7 +62,7 @@ var mainList = {
 							for(let i = 0; i < 4; i++){
 								let nameOfEmployer = prompt("Имя нового сотрудника?");
 
-								if((typeof(nameOfEmployer)) === 'string' && (typeof(nameOfEmployer)) != null && nameOfEmployer != ''){
+								if((typeof(nameOfEmployer)) === 'string' && (typeof(nameOfEmployer)) !== null && nameOfEmployer !== ''){
 										mainList.employers[i] = nameOfEmployer;
 								} 
 							}
@@ -71,7 +71,7 @@ var mainList = {
 					    
 	 chooseShopItems: function chooseShopItems(){
 	 	let items = prompt("Перечислите через запятую товары", "");
-	 	while((typeof(items)) != 'string' || items == null || items === ''){
+	 	while((typeof(items)) != 'string' || items === null || items === ''){
 	 		items = prompt("Перечислите через запятую товары", "");
 	 	}
 	 	mainList.shopItems = items.split(",");
@@ -80,11 +80,11 @@ var mainList = {
  	 },/*Теперь метод проверяет что items содержит непустую строку*/
 
  	 showShopItems: function showShopItems(){
- 	 	document.write("У нас вы можете купить: <br>");
+ 	 	document.body.innerHTML = "У нас вы можете купить: <br>";
  	 	mainList.shopItems.forEach(function(item, i, arr){
  	 		if(i == arr.length - 1){
- 	 			document.write((i+1)+") "+ item);
- 	 		} else { document.write((i+1)+") "+ item + "<br>");
+ 	 			document.body.innerHTML += ""+(i+1)+") "+ item;
+ 	 		} else { document.body.innerHTML += ''+(i+1)+") "+ item + "<br>";
  	 		}
  	 		
  	 	});
@@ -93,11 +93,17 @@ var mainList = {
  	 showShop: function showShop(){
  	 	console.log("Наш магазин включает в себя: \n");
  	 	for(var key in mainList){
- 	 		console.log(key + " = " + mainList[key] + "\n");	
- 	 		
+ 	 		if(mainList.hasOwnProperty(key)){
+ 	 			console.log(key + " = " + mainList[key] + "\n");	
+ 	 		}
+ 	
  	 	}
  	 }/*Метод выводящий в консоль все свойства и методы объекта mainList*/
-} //mainList
+}; //mainList
+
+//mainList.showShop();
+mainList.chooseShopItems();
+mainList.showShopItems();
 
 
 
@@ -111,16 +117,12 @@ let discount_value = document.getElementsByClassName('discount-value')[0];
 let isopen_value = document.getElementsByClassName('discount-value')[0];
 
 
-
 let goods_item = document.getElementsByClassName('goods-item')[0];
 
 let goods_item_btn = document.getElementsByTagName('button')[1];
 let count_budget_btn = document.getElementsByTagName('button')[2];
 let hire_employers_btn = document.getElementsByTagName('button')[3];
 
-console.log(goods_item_btn);
-console.log(count_budget_btn);
-console.log(hire_employers_btn);
 let choose_item = document.querySelector('.choose-item');
 let time_value = document.querySelector('.time-value');
 let count_budget_value = document.querySelector('.count-budget-value');
