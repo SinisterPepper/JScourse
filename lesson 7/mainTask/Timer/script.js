24325355c747a366326d2d95e1952cf100872182
Roman <roaltih@mail.ru>
2018-06-13 22:42:43 +0400

    Основное задание. 7-й урок

---
+++ lesson 7/mainTask/Timer/script.js
@@ -0,0 +1,13 @@
+let date = new Date();
+let div = document.getElementsByClassName('watch')[0];
+
+
+let param = {
+	hour: 'numeric',
+	minute: 'numeric',
+	second: 'numeric'
+};
+
+let timeIs = date.toLocaleString('ru', param);
+console.log(timeIs);
+div.textContent = timeIs;
