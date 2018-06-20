a6835ef53fa1d3ed48cc881e4dcd27f63cb0041d
Roman <roaltih@mail.ru>
2018-06-21 01:42:14 +0400

    12 lesson, mainTask

--- lesson 12/project v12/js/script.js
+++ lesson 12/project v12/js/script.js
@@ -64,8 +64,8 @@ window.addEventListener("DOMContentLoaded", function(){
 
 	function setClock(id, endtime){
 		let timer = document.getElementById(id),
-			hours = timer.querySelector('.hours');
-			minutes = timer.querySelector('.minutes');
+			hours = timer.querySelector('.hours'),
+			minutes = timer.querySelector('.minutes'),
 			seconds = timer.querySelector('.seconds');
 		let timeInterval;
 
@@ -253,7 +253,7 @@ window.addEventListener("DOMContentLoaded", function(){
     let persons = document.getElementsByClassName('counter-block-input')[0];
     let restDays = document.getElementsByClassName('counter-block-input')[1],
     	place = document.getElementById('select'),
-    	totalValue = document.getElementById('total');
+    	totalValue = document.getElementById('total'),
     	personsSum = 0,
     	daysSum = 0,
     	total = 0;
@@ -262,6 +262,7 @@ window.addEventListener("DOMContentLoaded", function(){
 
     persons.addEventListener('change', function() {
     	let reg = /^\d+$/;
+    	console.log(reg.test(this.value));
     	if(reg.test(this.value)){
     		personsSum = +this.value;
 			total = (daysSum + personsSum) * 4000;
@@ -280,6 +281,7 @@ window.addEventListener("DOMContentLoaded", function(){
 
     restDays.addEventListener('change', function() {
     	let reg = /^\d+$/;
+    	console.log(reg.test(this.value));
     	if(reg.test(this.value)){
     		daysSum = +this.value;
 	    	total = (daysSum + personsSum) * 4000;
@@ -292,18 +294,17 @@ window.addEventListener("DOMContentLoaded", function(){
     	} else {
     		this.value = '';
     		totalValue.innerHTML = 0;
-    	}
-
-    	
+    	}    	
     });
 
     place.addEventListener('change', function() {
-    	if (restDays.value === '' || persons.value === '') {
+    	if (restDays.value === '' || persons.value === '' || 
+	    		restDays.value === '0' || persons.value === '0') {
     		totalValue.innerHTML = 0;
     	} else {
     		let a = total;
     		totalValue.innerHTML = a * this.options[this.selectedIndex].value;
     	}
-    })
+    });
 
 });	
